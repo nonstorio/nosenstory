ae12b59d8cb04850a5d5c9a974d778a843cc9ab7
Rodion Borisov <vintprox@gmail.com>
2020-06-26 18:22:30 +0300

    Old files removed

--- ns/models/NsPlayer.py
+++
@@ -1,6 +0,0 @@
-from . import Model
-from peewee import BigIntegerField, IntegerField
-
-class NsPlayer(Model):
-    ref_id = BigIntegerField(unique = True)
-    rounds_played = IntegerField(default = 0)
