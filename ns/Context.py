629bf1a9a58bc734340c1f01d916f3defce58132
Rodion Borisov <vintprox@gmail.com>
2020-07-06 16:33:02 +0300

    Fix send_to_mgmt: designated to guild that the message was meant for.

--- ns/Context.py
+++ ns/Context.py
@@ -1,4 +1,5 @@
 from core.models import (
+    NsGuild,
     NsChannel,
     NsMgmt
 )
@@ -9,15 +10,19 @@ class Context(commands.Context):
     async def reply(self, content = None, **kwargs):
         mention = f'<@{self.author.id}>'
         return await self.send(f'{mention} ⦗ {content} ⦘' if content else mention, **kwargs)
+
     async def edit_reply(self, message, content = None, **kwargs):
         mention = f'<@{self.author.id}>'
         return await message.edit(content = f'{mention} ⦗ {content} ⦘' if content else mention, **kwargs)
+
     async def send_to_mgmt(self, content = None, ping = False, **kwargs):
         query = (NsMgmt
             .select()
-            .join(NsChannel)
+            .join_from(NsMgmt, NsGuild)
+            .join_from(NsMgmt, NsChannel)
             .where(
                 NsMgmt.user.is_null(),
+                NsMgmt.guild.ref_id == self.guild.id,
                 NsMgmt.channel.is_null(False)
             ))
         for nsmgmt in query:
